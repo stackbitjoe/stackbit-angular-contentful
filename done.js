411e1e29fa4686d80c249c0297af50e25301706b
stackbitjoe <joe@stackbit.com>
2022-06-16 09:25:49 -0400

    Testing ssg configuration

---
+++ done.js
@@ -0,0 +1,6 @@
+console.log('compiling...')
+console.log("'compiling...'")
+console.log("'compiled successfully'");
+console.log("compiled successfully");
+console.log('ready - started server on')
+console.log("'ready - started server on'")
